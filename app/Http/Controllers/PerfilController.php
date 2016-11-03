865f1f96ae29744b00b4e6954d63e6ee686f2828
Bove, Riccardo <Riccardo.Bove@experian.com>
2016-11-03 16:34:32 -0600

    Acoplo vista de perfil de Katherine

--- app/Http/Controllers/PerfilController.php
+++ app/Http/Controllers/PerfilController.php
@@ -6,17 +6,16 @@ use Illuminate\Http\Request;
 use DB;
 use App\Perfil;
 use App\Http\Requests;
+use Illuminate\Support\Facades\Auth;
 
 class PerfilController extends Controller
 {
     public function index()
     {
-		$user = DB::table('users')->where('id','1')->first();
-       // $user = DB::table('users')->get();
+        $loggedusr = Auth::id();
+		    $user = DB::table('users')->where('id',$loggedusr)->first();
+        // $user = DB::table('users')->get();
         //pasa por url a la vista
         return view('perfil.perfil', ['user' => $user]);
     }
 }
-
-
-
